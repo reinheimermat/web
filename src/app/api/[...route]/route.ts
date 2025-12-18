a7ec07eef1ad65572711c892a9fc441658acc01e
Matheus Reinheimer <mreinheimer0@gmail.com>
2025-12-18 20:49:54 -0300

    fix: Adjust hono

--- src/app/api/[...route]/route.ts
+++ src/app/api/[...route]/route.ts
@@ -1,7 +1,10 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+import routes from '../_routes'
 
 const app = new Hono().basePath('/api')
+
+app.route('/', routes)
 
 export const GET = handle(app)
 export const POST = handle(app)
